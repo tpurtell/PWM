5923371e368ae187add812cfda2f82de9310711d
BALEHOK <zhmurik@yandex.ru>
2016-05-23 21:07:29 +0300

    ensure the first word is retrieved from driver before any write operation

--- PwmLib/PWM.cs
+++ PwmLib/PWM.cs
@@ -5,33 +5,53 @@ namespace PwmLib
 {
     internal class PWM
     {
-        private DataHandler dh = new DataHandler();
-        private byte[] baseData = new byte[8];
+        private DataHandler _dh = new DataHandler();
+        private byte[] _baseData = new byte[8];
+        private bool _baseDataInitialized = false;
+
 
         public uint GetFrequency(ref int something, ref int frequency)
         {
             uint error = 0U;
-            dh.GetDataFromDriver(ESCAPEDATATYPE_ENUM.GET_SET_PWM_FREQUENCY, 8, ref error, ref baseData[0]);
+            _dh.GetDataFromDriver(ESCAPEDATATYPE_ENUM.GET_SET_PWM_FREQUENCY, 8, ref error, ref _baseData[0]);
             if (error != 0)
             {
                 return error;
             }
 
-            something = BitConverter.ToInt32(baseData, 0);
-            frequency = BitConverter.ToInt32(baseData, 4);
+            something = BitConverter.ToInt32(_baseData, 0);
+            frequency = BitConverter.ToInt32(_baseData, 4);
 
             return 0;
         }
 
         public uint SetFrequency(int frequency)
         {
+            EnsureBaseDataInitialized();
+
             byte[] b = BitConverter.GetBytes(frequency);
-            Array.Copy(b, 0, baseData, 4, 4);
+            Array.Copy(b, 0, _baseData, 4, 4);
 
             uint error = 0U;
-            dh.SendDataToDriver(ESCAPEDATATYPE_ENUM.GET_SET_PWM_FREQUENCY, 8, ref error, ref baseData[0]);
+            _dh.SendDataToDriver(ESCAPEDATATYPE_ENUM.GET_SET_PWM_FREQUENCY, 8, ref error, ref _baseData[0]);
 
             return error;
+        }
+
+        /// <summary>
+        /// we need to know first DWORD parameter of driver register before we write new frequency
+        /// so if are going to update PWM freq we need to query the current settings first
+        /// the first call to GetFrequency method will init _baseData
+        /// </summary>
+        private void EnsureBaseDataInitialized()
+        {
+            if (_baseDataInitialized)
+            {
+                return;
+            }
+
+            int a = 0, f = 0;
+            GetFrequency(ref a, ref f);
         }
     }
 }
