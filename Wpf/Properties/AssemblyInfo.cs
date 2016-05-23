33ff4266fc5cbc366506022b77a476dce5ea93c9
BALEHOK <zhmurik@yandex.ru>
2016-05-23 15:45:07 +0300

    assembly info

--- Wpf/Properties/AssemblyInfo.cs
+++ Wpf/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Wpf")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("PWM Tool")]
+[assembly: AssemblyDescription("Intel PWN frequency control")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Wpf")]
+[assembly: AssemblyProduct("PWM Tool")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
